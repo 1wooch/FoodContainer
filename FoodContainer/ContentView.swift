e957456abe4e319b5cbbbf26d9894afd24763dc5
wwc <onechoo1106@gmail.com>
2023-08-19 19:56:06 +1000

    Initial Commit

---
+++ FoodContainer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FoodContainer
+//
+//  Created by 최원우 on 19/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodContainer/FoodContainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodContainerApp.swift
+//  FoodContainer
+//
+//  Created by 최원우 on 19/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodContainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
